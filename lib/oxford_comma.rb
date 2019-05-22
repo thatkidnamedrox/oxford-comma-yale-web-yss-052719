fcb7d08a1792b8246fdf2ddb24f02b207c7f669c
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 04:59:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  size = array.size()
+  if size < 2
+    return array.join()
+  elsif size == 2
+    return array.join(" and ")
+  else
+    copy = []
+    array.each do |element|
+      copy << element.dup
+    end
+    last_element = copy.pop()
+    string = copy.join(", ")
+    string.concat(", and #{last_element}")
+    return string
+  end
 
 end
